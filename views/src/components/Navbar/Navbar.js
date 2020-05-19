51f823195be8348c9b65ff56a0b7b17594b403b1
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-05-19 16:04:44 -0700

    initial commit, created homepage and navbar

---
+++ views/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>Login</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+};
+export default Navbar;
---
+++ views/src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+const Homepage = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default Homepage;
